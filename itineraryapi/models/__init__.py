785cae46958806a949c7e6f05d4da21bb15924a9
Abby Paradise <162839518+asparadise33@users.noreply.github.com>
2025-01-25 11:25:34 -0600

    changes made to trip location model

--- itineraryapi/models/__init__.py
+++ itineraryapi/models/__init__.py
@@ -1,3 +1,4 @@
 from .user import User
 from .location import Location
 from .travel_mode import TravelMode
+from .trip_location import TripLocation
--- itineraryapi/models/trip_location.py
+++ itineraryapi/models/trip_location.py
@@ -1,5 +1,6 @@
 from django.db import models
+from .location import Location
 
 class TripLocation(models.Model):
-    trip_id = models.ForeignKey(Trip, on_delete=models.CASCADE, related_name='locationTrips')
+   # trip_id = models.ForeignKey(Trip, on_delete=models.CASCADE, related_name='locationTrips')
     location_id = models.ForeignKey(Location, on_delete=models.CASCADE, related_name='tripLocations')
