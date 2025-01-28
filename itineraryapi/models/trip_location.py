5e6fccf168141ed59373b3af5383cac9f243c310
Abby Paradise <162839518+asparadise33@users.noreply.github.com>
2025-01-27 18:04:37 -0600

    saving migrations

--- itineraryapi/models/trip_location.py
+++ itineraryapi/models/trip_location.py
@@ -1,6 +1,7 @@
 from django.db import models
 from .location import Location
+from .trip import Trip
 
 class TripLocation(models.Model):
-   # trip_id = models.ForeignKey(Trip, on_delete=models.CASCADE, related_name='locationTrips')
-    location_id = models.ForeignKey(Location, on_delete=models.CASCADE, related_name='tripLocations')
+    trip = models.ForeignKey(Trip, on_delete=models.CASCADE, related_name='locationTrips')
+    location = models.ForeignKey(Location, on_delete=models.CASCADE, related_name='tripLocations')
